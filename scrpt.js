71334df12ca349aa952c00af16f6f473d257acfb
sj_kumar <sonuyadav0521@gmail.com>
2018-07-02 15:46:09 +0530

    small modifications

--- scrpt.js
+++ scrpt.js
@@ -1,7 +1,7 @@
 
 //****************     Login credential validation   *******************
 function validate(){
-  var roll_no=document.getElementById("login_id").value;
+  var roll=document.getElementById("login_id").value;
   var pass=document.getElementById("password").value;
   if(roll=="")
   {alert("Please enter username"); return false;}
@@ -56,10 +56,10 @@ function validateemail(inputText)
   }
 }
 
-// //*****************Angular Js***********************//
+//*****************Angular Js***********************//
 // var app=angular.module('myApp',[]);
 // app.controller('myCtrl',function($scope,$http){
 //   $http.get('').then(function(response){
-//     $scope.=response.data;
+//     $scope=response.data;
 //   })
 // })
